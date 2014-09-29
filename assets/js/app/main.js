f63f1024cbe392f9ec178fc5bae89b84d9c5e8e6
Ray Arvin <rayarvin@gmail.com>
2014-09-30 04:08:49 +0800

    finally

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -31,9 +31,21 @@ require(["jquery"], function($) {
 	            event.preventDefault();
 	        }
 	    });
+
+
+		$('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		        var target = $(this.hash);
+		        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		        if (target.length) {
+		            $('html,body').animate({
+		                scrollTop: target.offset().top
+		            }, 1000);
+		            return false;
+		        }
+		    }
+		});
 	});
-
-
 });
 
 require(["jquery","owl.carousel"], function($) {
